131515ce60c42f12835118aad9496f7b5693e943
topping <reyna11@naver.com>
2023-10-15 21:38:21 +0900

    1015 5장 2차원 정방 행렬 배열

---
+++ src/Reference_type/_05_RectangleArrayDefinition.java
@@ -0,0 +1,26 @@
+package Reference_type;
+
+public class _05_RectangleArrayDefinition {
+    public static void main(String[] args) {
+        // 배열 선언1
+        int[][] array1 = new int[3][4];
+        int[][] array2;
+
+        // 배열 선언2
+        int array3[][] = new int[3][4];
+        int array4[][];
+        array4 = new int[3][4];
+
+        // 배열 선언3
+        int[] array5[] = new int[3][4];
+        int[] array6[];
+        array6 = new int[3][4];
+
+        // 기본, 참조 자료형 배열 선언 방법
+        boolean[][] array7 = new boolean[3][4];
+        int[][] array8 = new int[3][4];
+        double[][] array9 = new double[3][4];
+        String[][] array10 = new String[3][4];
+    }
+
+}
