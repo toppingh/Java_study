5db067566ddc98e55a2b1e0897b4316606432a9f
topping <reyna11@naver.com>
2023-11-19 20:26:50 +0900

    1119 5장 연습문제 완료

--- src/Reference_type/Question.java
+++ src/Reference_type/Question.java
@@ -4,25 +4,60 @@ import java.util.Arrays;
 
 public class Question {
     public static void main(String[] args) {
-        double[] a = {1.2, 3.4, 5.6};
-        double[] b = a;
-        b[0] = 7.8;
-        System.out.println(Arrays.toString(a));
-        System.out.println(Arrays.toString(b));
+        double[] ab = {1.2, 3.4, 5.6};
+        double[] bc = ab;
+        bc[0] = 7.8;
+        System.out.println(Arrays.toString(ab));
+        System.out.println(Arrays.toString(bc));
 
-        int[] c = new int[]{1, 2, 3, 4, 5};
-        for (int i: c) {
-            System.out.println(c[i-1]);
+        int[] cd = new int[]{1, 2, 3, 4, 5};
+        for (int i: cd) {
+            System.out.println(cd[i-1]);
         }
 
-        int[][] d = new int[2][];
-        d[0] = new int[]{1, 3, 5};
-        d[1] = new int[]{7, 9};
+        int[][] de = new int[2][];
+        de[0] = new int[]{1, 3, 5};
+        de[1] = new int[]{7, 9};
 
-        for (int i = 0; i < d.length; i++) { // i = 0
-            for (int j = 0; j < d[i].length; j++) {
-                System.out.println(d[i][j]);
+        for (int i = 0; i < de.length; i++) { // i = 0
+            for (int j = 0; j < de[i].length; j++) {
+                System.out.println(de[i][j]);
             }
         }
+
+        String a = "방가";
+        String b = new String("방가");
+        String c = "방가";
+        String d = new String("방가");
+        String e = "방가";
+        String f = new String("방가");
+
+        System.out.println(a == b);
+        System.out.println(a == c);
+        System.out.println(a == d);
+        System.out.println(a == e);
+        System.out.println(a == f);
+        System.out.println();
+
+        System.out.println(b == c);
+        System.out.println(b == d);
+        System.out.println(b == e);
+        System.out.println(b == f);
+        System.out.println();
+
+        String str = "내 이름은 [홍길동]입니다. 나이는 [15]살 입니다";
+        String name;
+        int age;
+
+        int start1 = str.indexOf("[");
+        int end1 = str.indexOf("]");
+        name = str.substring(start1 + 1, end1);
+
+        int start2 = str.lastIndexOf("[");
+        int end2 = str.lastIndexOf("]");
+        age = Integer.parseInt(str.substring(start2 + 1, end2));
+
+        System.out.println(name);
+        System.out.println(age);
     }
 }
