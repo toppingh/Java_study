60e29759f7d827a35e66f6d7cb1798a223a3bff5
topping <reyna11@naver.com>
2023-12-02 23:47:58 +0900

    1202 Java 코딩 테스트 1번째

---
+++ src/Test/_01_FirstDay.java
@@ -0,0 +1,42 @@
+package Test;
+
+//class Solution {
+//    public int solution(int num1, int num2) {
+//        int answer = 0;
+//        return answer;
+//    }
+//}
+
+// 두 수의 곱
+class Solution {
+    public int solution(int num1, int num2) {
+        int answer = num1 * num2;
+        return answer;
+    }
+}
+
+// 두 수의 차
+class Solution1 {
+    public int solution(int num1, int num2) {
+        return num1 - num2;
+    }
+}
+
+// 두 수의 합
+class Solution2 {
+    public int solution(int num1, int num2) {
+        int answer = num1 + num2;
+        return answer;
+    }
+}
+
+// 몫 구하기
+class Solution3 {
+    public int solution(int num1, int num2) {
+        int answer = num1 / num2;
+        return answer;
+    }
+}
+
+public class _01_FirstDay {
+}
