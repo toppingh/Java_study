a850678c6b6027d63e26572f9e4add67a4a5ff22
topping <reyna11@naver.com>
2023-12-13 18:48:28 +0900

    1213 같은 패키지에서 클래스 접근 지정자와 생성자 접근 지정자 실습

---
+++ src/Chapter8_Class_Outside/AccessModifierOfClass_1/A.java
@@ -0,0 +1,5 @@
+package Chapter8_Class_Outside.AccessModifierOfClass_1;
+
+public class A { // public 클래스
+    // public 기본 생성자를 컴파일러가 자동으로 추가
+}
---
+++ src/Chapter8_Class_Outside/AccessModifierOfClass_1/C.java
@@ -0,0 +1,7 @@
+package Chapter8_Class_Outside.AccessModifierOfClass_1;
+
+public class C { // public 클래스
+    C() {
+        // default 생성자 내가 직접 생성!
+    }
+}
