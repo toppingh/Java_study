162fecf5ce59b73f24feb183a14229534f8c7692
topping <reyna11@naver.com>
2023-12-14 02:10:18 +0900

    1214 인스턴스 필드와 정적 필드의 활용 방법 실습

---
+++ src/Chapter9_Modifier/_01_StaticField_1.java
@@ -0,0 +1,22 @@
+package Chapter9_Modifier;
+
+class A {
+    int m = 3; // 인스턴스 필드
+    static int n = 5; // 정적 필드
+}
+
+public class _01_StaticField_1 {
+    public static void main(String[] args) {
+        // 인스턴스 필드 활용 방법(객체를 생성한 후 사용 가능)
+        A a1 = new A();
+        System.out.println(a1.m);
+
+        // 정적 필드 활용 방법
+        // 1. 객체 생성 없이 클래스명으로 바로 활용
+        System.out.println(A.n);
+
+        // 2. 객체 생성 후 활용 (권장x)
+        A a2 = new A();
+        System.out.println(a2.n);
+    }
+}
