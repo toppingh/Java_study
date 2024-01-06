d7b93de487dcbeec2082b86a2bca118f4ad06c5f
topping <reyna11@naver.com>
2024-01-06 23:54:37 +0900

    0106 12번째 코딩테스트 실습

--- src/Test/_11_11Day.java
+++ src/Test/_11_11Day.java
@@ -1,6 +1,7 @@
 package Test;
 
 import java.util.Arrays;
+import java.util.*;
 
 // 중복된 숫자 개수 (내 풀이)
 class Solution31 {
@@ -77,6 +78,45 @@ class Solution37 {
             if (i > height) answer++;
         }
         return answer;
+    }
+}
+
+// 배열 뒤집기 (내 풀이)
+class Solution38 {
+    public int[] solution(int[] num_list) {
+        int[] answer = new int [num_list.length];
+        for(int i = 0; i <= num_list.length - 1; i++) {
+            answer[i] = num_list[num_list.length - 1 - i];
+        }
+        return answer;
+    }
+}
+
+// 최댓값 만들기(1) (내 풀이)
+class Solution39 {
+    public int solution(int[] numbers) {
+        int answer = 0;
+        int max = 0;
+
+        for(int i = 0; i < numbers.length; i++) {
+            for (int j = i + 1; j < numbers.length; j++) {
+                if (numbers[i] * numbers[j] > max) {
+                    max = numbers[i] * numbers[j];
+                }
+            }
+        }
+        answer = max;
+        return answer;
+    }
+}
+
+// 최댓값 만들기(1) - 간단ver
+class Solution40 {
+    public int solution(int[] numbers) {
+        int answer = 0;
+        Arrays.sort(numbers);
+
+        return numbers[numbers.length - 1] * numbers[numbers.length - 2];
     }
 }
 public class _11_11Day {
