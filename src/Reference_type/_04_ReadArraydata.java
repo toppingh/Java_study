fedd94daf134910e0c066a885ec55ebbe0db0c45
topping <reyna11@naver.com>
2024-08-10 18:33:36 +0900

    0810 commit

--- src/Reference_type/_04_ReadArraydata.java
+++ src/Reference_type/_04_ReadArraydata.java
@@ -4,31 +4,43 @@ import java.util.Arrays;
 
 public class _04_ReadArraydata {
     public static void main(String[] args) {
-        int[] array = new int[]{3, 4, 5, 6, 7};
-
-        // 배열 길이
-        System.out.println(array.length);
-
-        // 출력 첫번째 방법
-        System.out.print(array[0]+ " ");
-        System.out.print(array[1]+ " ");
-        System.out.print(array[2]+ " ");
-        System.out.print(array[3]+ " ");
+        // 2차원 데이터의 배열의 길이
+        int[][] arr1 = new int[2][3];
+        System.out.println(arr1.length); // 행 개수, 2
+        System.out.println(arr1[0].length); // 첫번째 행의 열 개수, 3
+        System.out.println(arr1[1].length); // 두번째 행의 열 개수, 3
         System.out.println();
 
-        // 출력 두번째 방법
-        for (int i = 0; i < array.length; i++) {
-            System.out.println(array[i]);
+        int[][] arr2 = new int [][] {{1, 2}, {3, 4, 5}};
+        System.out.println(arr2.length); // 2
+        System.out.println(arr2[0].length); // 2
+        System.out.println(arr2[1].length); // 3
+        System.out.println();
+
+        // 2차원 배열 출력
+        System.out.print(arr2[0][0] + " "); // 1
+        System.out.print(arr2[0][1] + " "); // 2
+        System.out.println();
+        System.out.print(arr2[1][0] + " "); // 3
+        System.out.print(arr2[1][1] + " "); // 4
+        System.out.print(arr2[1][2]); // 5
+        System.out.println();
+
+        // 이중 for문
+        for (int i = 0; i < arr2.length; i++) {
+            for (int j = 0; j < arr2[i].length; j++) {
+                System.out.print(arr2[i][j] + " ");
+            }
+            System.out.println();
         }
         System.out.println();
 
-        // 출력 세번째 방법
-        for (int k: array) {
-            System.out.println(k + " ");
+        // 이중 for-each문
+        for (int[] arr: arr2) {
+            for (int k:arr) {
+                System.out.print(k + " ");
+            }
+            System.out.println();
         }
-        System.out.println();
-
-        // 출력 네번째 방법
-        System.out.println(Arrays.toString(array));
     }
 }
